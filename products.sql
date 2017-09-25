a2125e3cbc9fc2d574ba789d0742f26eeff131a3
NancyDLL <nano14@mac.com>
2017-09-25 15:10:31 -0500

    created products

---
+++ products.sql
@@ -0,0 +1,31 @@
+USE bamazon_db;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Crystal Wand", "Mage", 1550, 15 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Viking Sword", "Warrior", 750, 7);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Horned Helmit", "Warrior", 400, 5 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Invibility Potion", "Mage", 2575, 1);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bow", "Elf", 750, 11);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Quiver", "Elf", 50, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Arrow", "Elf", 5, 1000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Sheild", "Warrior", 1000, 8);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Robe", "Mage", 1000, 8);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Ring of Power", "Elf", 5000, 1);
