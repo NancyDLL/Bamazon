05043bb5ff6f2915a1f0d1d42dabbdfc10602b10
NancyDLL <nano14@mac.com>
2017-09-25 15:30:42 -0500

    started customer js file

---
+++ bamazonCustomer.js
@@ -0,0 +1,5 @@
+//The inquirer module will be required to be able to ask questions.
+var inquirer = require("inquire");
+
+//mySQL will be required to be able to update the db of products.
+var mysql = require("mysql");
