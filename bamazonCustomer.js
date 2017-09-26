4139f935c5ce878b819cef17752e79ed24987ebc
NancyDLL <nano14@mac.com>
2017-09-26 08:56:22 -0500

    added the module to display db in a table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,6 +4,9 @@ var inquirer = require("inquirer");
 //mySQL will be required to be able to update the db of products.
 var mysql = require("mysql");
 
+//use this to display all the products in the db
+require("console.table");
+
 // create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
@@ -27,6 +30,40 @@ connection.connect(function(err) {
 function listProducts() {
 	connection.query("SELECT * FROM products", function(err, res) {
 		if (err) throw err;
-		console.log(res);
+		console.log(" ");
+		console.log("                  THE SUPER QUEST ONLINE STORE");
+		console.log("                 Below is our current inventory.");
+		console.log("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+		console.table(res);
+		console.log("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+		console.log(" ");
+	//run the function for customer selection, with all the products.
+	customerProdSelect(res);
 	});
+}
+
+// List products for the customer to select from
+function customerProdSelect(inventory) {
+  // Prompts user for what they would like to purchase
+  inquirer.prompt([
+      {
+        type: "list",
+        name: "productList",
+        message: "Select from the list which product that you would like to buy.",
+        choices: [res]
+      }
+    ]).then(function(val) {
+      var product = checkInventory(choiceId, inventory);
+
+      //Prompt the customer for a desired quantity
+      if (product) {
+        // Pass the chosen product to promptCustomerForQuantity
+        promptCustomerForQuantity(product);
+      }
+      else {
+        // Otherwise let them know the item is not in the inventory, re-run loadProducts
+        console.log("\nThat item is not in the inventory.");
+        loadProducts();
+      }
+    });
 }
